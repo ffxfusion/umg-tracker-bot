ff5450d7c3a8f4615f88e958b304e1e617eeafe0
syaro <70029137+ffxfusion@users.noreply.github.com>
2024-04-07 02:15:09 +0100

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,15 +1,16 @@
 const server = require("../schema/guild.js");
 const mongoose = require("mongoose");
+const util = require("../util.js");
 
 const { ChannelType, PermissionsBitField } = require("discord.js");
 
 module.exports = async (client, guild) => {
-    console.log(`[GUILD] ${client.user.tag} has joined ${guild.name} (${guild.id}).`);
+    util.logger(`[GUILD] ${client.user.tag} has joined ${guild.name} (${guild.id}).`);
 
     const c = await guild.channels.cache.find(c => c.type === ChannelType.GuildText && guild.members.me.permissions.has(PermissionsBitField.Flags.SendMessages));
 
     if (!c) {
-        console.log(`[INFO] ${guild.name} (${guild.id}) has no text channels, skipping welcome message. lol`);
+        util.logger(`[INFO] ${guild.name} (${guild.id}) has no text channels, skipping welcome message. lol`);
     } else {
         const msg = "Thanks for adding me to your server, send me (syaro) a DM if you encounter any bugs as this bot is still in development. \n\nTo get started, run the `setup` command. \n\nIf you need help, run the `help` command.";
         c.send(msg)
@@ -35,13 +36,23 @@ module.exports = async (client, guild) => {
                     webhookToken: String,
                     webhookID: String,
                 },
+                raretracker: {
+                    channelID: String,
+                    webhookToken: String,
+                    webhookID: String,
+                },
+                voidtracker: { //wip
+                    channelID: String,
+                    webhookToken: String,
+                    webhookID: String,
+                },
                 userspawn: []
             }
         });
     } else {
-        return console.log(`[DATABASE] ${guild.name} (${guild.id}) has been loaded from the database.`);
+        return util.logger(`[DATABASE] ${guild.name} (${guild.id}) has been loaded from the database.`);
     }
 
-    await guildProfile.save().catch(err => console.log(err));
-    console.log(`[DATABASE] ${guild.name} (${guild.id}) has been saved to the database.`);
+    await guildProfile.save().catch(err => util.logger(err));
+    util.logger(`[DATABASE] ${guild.name} (${guild.id}) has been saved to the database.`);
 }
