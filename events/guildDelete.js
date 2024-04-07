1b0a195e5d0e7354ca3c20de70c5fdcb4e026d19
syaro <70029137+ffxfusion@users.noreply.github.com>
2024-04-07 02:14:56 +0100

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,8 +1,9 @@
 const server = require("../schema/guild.js");
+const util = require("../util.js");
 
 module.exports = async(client, guild) => {
 
     await server.findOneAndDelete({ guildId: guild.id });
 
-    console.log(`[GUILD] ${client.user.tag} has left ${guild.name} (${guild.id}) (removed from database).`);
+    util.logger(`[GUILD] ${client.user.tag} has left ${guild.name} (${guild.id}) (removed from database).`);
 }
