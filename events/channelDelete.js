cfe1cde38c7641ab37b1ffb4cfdfaf875c06305b
syaro <70029137+ffxfusion@users.noreply.github.com>
2024-04-07 02:15:19 +0100

    Update channelDelete.js

--- events/channelDelete.js
+++ events/channelDelete.js
@@ -2,31 +2,31 @@ const server = require("../schema/guild.js");
 const util = require("../util.js");
 
 module.exports = async (client, channel) => {
-    let database = await server.findOne({ guildId: channel.guild.id }).catch(err => console.log(err));
+    let database = await server.findOne({ guildId: channel.guild.id }).catch(err => util.logger(err));
 
     if (!database || isNaN(database.config.rarespawn.channelID)) return;
     
     const logMsg = `[CHANNEL DELETE] - ${channel.name} (${channel.id}) was deleted in ${channel.guild.name} (${channel.guild.id}) (removed from database)`;
 
-    if (util.handleChannelDeleteAndLog(database.config.rarespawn, channel, database, logMsg)) {
-        return;
-    }
-    
-    if (util.handleChannelDeleteAndLog(database.config.globaltracker, channel, database, logMsg)) {
-        return;
+    const configs = ["rarespawn", "globaltracker", "raretracker"];
+
+    for (let config of configs) {
+        if (util.handleChannelDeleteAndLog(database.config[config], channel, database, logMsg)) {
+            return;
+        }
     }
 
     const user = database.config.userspawn.find(v => v.channelID === channel.id);
 
     if (user) {
-        console.log(`[CHANNEL DELETE] - ${channel.name} (${channel.id}) was deleted in ${channel.guild.name} (${channel.guild.id}) (removed from database)`);
+        util.logger(logMsg);
         return await server.findOneAndUpdate({ guildId: channel.guild.id }, {
             $pull: {
                 "config.userspawn": {
                     channelID: channel.id
                 }
             }
-        }).catch(err => console.log(err));
+        }).catch(err => util.logger(err));
     }
     return;
 }
