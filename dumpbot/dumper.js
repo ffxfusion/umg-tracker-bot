4f54ec40a2bf24af4a28debf1ce4395c12fc2f59
syaro <70029137+syaro0810@users.noreply.github.com>
2024-06-06 23:12:22 +0100

    Update dumper.js

--- dumpbot/dumper.js
+++ dumpbot/dumper.js
@@ -1,26 +1,40 @@
-const { Client, WebhookClient } = require("discord.js-selfbot-v13")
+const { Client: selfbot, WebhookClient } = require("discord.js-selfbot-v13");
+const superchills = require("../superchills.js");
 
-const client = new Client();
+const client = new selfbot();
 
 client.on("ready", async () => {
     console.log("selfbot online.");
 });
 
-const webhookUrls = {
-    rarespawn: "proxy server webhook url here",
-    normalTracker: "proxy server webhook url here",
-    alteredTracker: "proxy server webhook url here",
-    superAlteredTracker: "proxy server webhook url here",
-    giganticTracker: "proxy server webhook url here",
-    tinyTracker: "proxy server webhook url here",
-    evilTracker: "proxy server webhook url here"
+const rarespawn = new WebhookClient({ url: "proxy regular rarespawn webhook" });
+const superchillUrl = {
+    rarespawn: "superchill proxy",
+    normalTracker: "superchill proxy",
+    alteredTracker: "superchill proxy",
+    superAlteredTracker: "superchill proxy",
+    giganticTracker: "superchill proxy",
+    tinyTracker: "superchill proxy",
+    evilTracker: "superchill proxy"
 };
-  
-const hooks = {};
-  
-for (const [key, url] of Object.entries(webhookUrls)) {
-    hooks[`${key}Hook`] = new WebhookClient({ url });
+
+const ids = {};
+
+for (const [key, url] of Object.entries(superchillUrl)) {
+    ids[`${key}Hook`] = new WebhookClient({ url });
 }
+
+const rarities = [...Object.values(superchills.world1), ...Object.values(superchills.world2), ...Object.values(superchills.w2Caves)].flat();
+
+const trackers = {
+    "1059846328772993064": { authorId: "1246216620880298065", hook: ids.rarespawnHook},
+    "1059846357860491376": { authorId: "1088664394130534421", hook: ids.normalTrackerHook },
+    "1173767047080054784": { authorId: "1173767219549843527", hook: ids.alteredTrackerHook },
+    "1173773186240888842": { authorId: "1173773260542988483", hook: ids.superAlteredTrackerHook },
+    "1173778041600757760": { authorId: "1173778285767950436", hook: ids.giganticTrackerHook },
+    "1173778098379034705": { authorId: "1173779194933682197", hook: ids.tinyTrackerHook },
+    "1173778128921960518": { authorId: "1173779668030206032", hook: ids.evilTrackerHook }
+};
 
 let embedData = {
     rare: false
@@ -28,24 +42,29 @@ let embedData = {
 
 client.on("messageCreate", async (message) => {
     const guildId = "1059506407826788393";
-    const trackers = {
-        "1059846328772993064": { authorId: "1088664915457347594", hook: hooks.rarespawnHook },
-        "1059846357860491376": { authorId: "1088664394130534421", hook: hooks.normalTrackerHook },
-        "1173767047080054784": { authorId: "1173767219549843527", hook: hooks.alteredTrackerHook },
-        "1173773186240888842": { authorId: "1173773260542988483", hook: hooks.superAlteredTrackerHook },
-        "1173778041600757760": { authorId: "1173778285767950436", hook: hooks.giganticTrackerHook },
-        "1173778098379034705": { authorId: "1173779194933682197", hook: hooks.tinyTrackerHook },
-        "1173778128921960518": { authorId: "1173779668030206032", hook: hooks.evilTrackerHook }
-    };
 
     if (message.guild.id === guildId) {
+        if (message.channel.id === "1059846328772993064" && message.author.id === "1246216620880298065") {
+            if (message.content) {
+                embedData.rare = true;
+            }
+    
+            rarespawn.send({ content: embedData.rare ? "ping" : " ", embeds: [message.embeds[0]] });
+        }
+
         const tracker = trackers[message.channel.id];
         if (tracker && message.author.id === tracker.authorId) {
             if (message.content && tracker === trackers["1059846328772993064"]) {
                 embedData.rare = true;
             }
-
-            tracker.hook.send({ content: embedData.rare ? "ping" : " ", embeds: [message.embeds[0]] });
+            
+            const title = message.embeds[0].title;
+            const titleWithoutSpawned = title.replace(/ spawned./g, "");
+            const titleWithoutDot = title.endsWith(".") ? title.slice(0, -1) : title;
+            
+            if ((title.includes("spawned.") && rarities.includes(titleWithoutSpawned)) || rarities.includes(titleWithoutDot)) {
+                tracker.hook.send({ content: embedData.rare ? "ping" : " ", embeds: [message.embeds[0]] });
+            }
         }
     }
 
@@ -54,4 +73,4 @@ client.on("messageCreate", async (message) => {
     };
 });
 
-client.login("token here");
+client.login('alt token');
